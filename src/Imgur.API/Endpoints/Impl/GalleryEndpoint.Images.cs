964b90cf22d3b93a6f1c5221f1bf923fb13f7373
Damien Dennehy <damien@dennehy.me>
2015-12-31 14:01:47 +0000

    Finished GalleryEndpoint image methods. Minor cleanup of appveyor file.

--- src/Imgur.API/Endpoints/Impl/GalleryEndpoint.Images.cs
+++ src/Imgur.API/Endpoints/Impl/GalleryEndpoint.Images.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Imgur.API.Models;
+using Imgur.API.Models.Impl;
 
 namespace Imgur.API.Endpoints.Impl
 {
@@ -10,10 +12,25 @@ namespace Imgur.API.Endpoints.Impl
         ///     Get additional information about an image in the gallery.
         /// </summary>
         /// <param name="imageId">The image id.</param>
+        /// <exception cref="ArgumentNullException">
+        ///     Thrown when a null reference is passed to a method that does not accept it as a
+        ///     valid argument.
+        /// </exception>
+        /// <exception cref="ImgurException">Thrown when an error is found in a response from an Imgur endpoint.</exception>
+        /// <exception cref="MashapeException">Thrown when an error is found in a response from a Mashape endpoint.</exception>
         /// <returns></returns>
         public async Task<IGalleryImage> GetGalleryImageAsync(string imageId)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(imageId))
+                throw new ArgumentNullException(nameof(imageId));
+
+            var url = $"gallery/image/{imageId}";
+
+            using (var request = RequestBuilder.CreateRequest(HttpMethod.Get, url))
+            {
+                var image = await SendRequestAsync<GalleryImage>(request);
+                return image;
+            }
         }
     }
 }

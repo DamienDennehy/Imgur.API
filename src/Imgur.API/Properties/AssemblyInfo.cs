7303dc57fb99c717ece7a895072ab0309e97f5d8
Damien Dennehy <damien@dennehy.me>
2016-01-11 11:11:59 -0700

    Converted project type to Class Library Package

--- src/Imgur.API/Properties/AssemblyInfo.cs
+++ src/Imgur.API/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,31 +6,12 @@ using System.Runtime.CompilerServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("Imgur.API")]
-[assembly:
-    AssemblyDescription(
-        "Imgur.API is a .NET implementation of Imgur's v3 API. It supports Imgur's free and Mashape's commercial API endpoints."
-        )]
+[assembly: AssemblyDescription("Imgur.API is a .NET implementation of Imgur's v3 API.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Damien Dennehy")]
 [assembly: AssemblyProduct("Imgur.API")]
 [assembly: AssemblyCopyright("Copyright © 2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
 [assembly: InternalsVisibleTo("Imgur.API.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0.0")]

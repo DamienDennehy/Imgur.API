29c690cbd412bb20bc91d067c4b56b181107362b
Damien Dennehy <damien.dennehy@genesisautomation.healthcare>
2018-06-24 20:19:07 +0100

    Removed models.

--- src/Imgur.API/RequestBuilders/RequestBuilderBase.cs
+++ src/Imgur.API/RequestBuilders/RequestBuilderBase.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
-using Imgur.API.Enums;
 
 namespace Imgur.API.RequestBuilders
 {
@@ -17,24 +14,6 @@ namespace Imgur.API.RequestBuilders
                 throw new ArgumentNullException(nameof(url));
 
             return new HttpRequestMessage(httpMethod, url);
-        }
-
-        internal static HttpRequestMessage ReportItemRequest(string url, ReportReason reason)
-        {
-            if (string.IsNullOrWhiteSpace(url))
-                throw new ArgumentNullException(nameof(url));
-
-            var parameters = new Dictionary<string, string>
-            {
-                {nameof(reason), ((int) reason).ToString()}
-            };
-
-            var request = new HttpRequestMessage(HttpMethod.Post, url)
-            {
-                Content = new FormUrlEncodedContent(parameters.ToArray())
-            };
-
-            return request;
         }
     }
 }
--- test/Imgur.API.Tests/RequestBuilderTests/RequestBuilderBaseTests.cs
+++ test/Imgur.API.Tests/RequestBuilderTests/RequestBuilderBaseTests.cs
@@ -44,33 +44,5 @@ namespace Imgur.API.Tests.RequestBuilderTests
             var argNullException = (ArgumentNullException) exception;
             Assert.Equal(argNullException.ParamName, "url");
         }
-
-        [Fact]
-        public async Task ReportItemRequest_Equal()
-        {
-            var client = new ImgurClient("123", "1234");
-            var requestBuilder = new CommentRequestBuilder();
-
-            var mockUrl = $"{client.EndpointUrl}comment/XysioD/report";
-            var request = RequestBuilderBase.ReportItemRequest(mockUrl, ReportReason.Abusive);
-
-            Assert.NotNull(request);
-            var expected = "reason=3";
-
-            Assert.Equal(expected, await request.Content.ReadAsStringAsync().ConfigureAwait(false));
-            Assert.Equal("https://api.imgur.com/3/comment/XysioD/report", request.RequestUri.ToString());
-            Assert.Equal(HttpMethod.Post, request.Method);
-        }
-
-        [Fact]
-        public void ReportItemRequest_WithUrlNull_ThrowsArgumentNullException()
-        {
-            var exception = Record.Exception(() => RequestBuilderBase.ReportItemRequest(null, ReportReason.Abusive));
-            Assert.NotNull(exception);
-            Assert.IsType<ArgumentNullException>(exception);
-
-            var argNullException = (ArgumentNullException) exception;
-            Assert.Equal(argNullException.ParamName, "url");
-        }
     }
 }

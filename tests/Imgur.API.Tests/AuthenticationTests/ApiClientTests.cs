a5a5bf4783e94d4869a1c75f708ce5013c5aa776
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2020-08-04 00:27:44 +0100

    Added ApiClient Tests.

---
+++ tests/Imgur.API.Tests/AuthenticationTests/ApiClientTests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Imgur.API.Authentication;
+using Imgur.API.Models;
+using Xunit;
+
+namespace Imgur.API.Tests.AuthenticationTests
+{
+    public class ApiClientTests
+    {
+        [Fact]
+        public void ClientId_SetByConstructor_AreEqual()
+        {
+            var client = new ApiClient("ClientId123", "ClientSecret123");
+
+            Assert.Equal("ClientId123", client.ClientId);
+        }
+
+        [Fact]
+        public void ClientId_SetNullByConstructor_ThrowArgumentNullException()
+        {
+            var exception = Record.Exception(() => new ApiClient(null));
+            Assert.NotNull(exception);
+            Assert.IsType<ArgumentNullException>(exception);
+
+            var argNullException = (ArgumentNullException)exception;
+            Assert.Equal("clientId", argNullException.ParamName);
+        }
+
+        [Fact]
+        public void ClientSecret_SetByConstructor_AreEqual()
+        {
+            var client = new ApiClient("ClientId123", "ClientSecret123");
+
+            Assert.Equal("ClientSecret123", client.ClientSecret);
+        }
+
+        [Fact]
+        public void ClientSecret_SetNullByConstructor_ThrowArgumentNullException()
+        {
+            var exception = Record.Exception(() => new ApiClient("ClientId", clientSecret: null));
+            Assert.NotNull(exception);
+            Assert.IsType<ArgumentNullException>(exception);
+
+            var argNullException = (ArgumentNullException)exception;
+            Assert.Equal("clientSecret", argNullException.ParamName);
+        }
+
+        [Fact]
+        public void OAuth2Token_SetByClientIdAndSecretConstructor_AreSame()
+        {
+            var oAuth2Token = new OAuth2Token
+            {
+                AccessToken = Guid.NewGuid().ToString(),
+                AccountId = Guid.NewGuid().ToString(),
+                AccountUsername = Guid.NewGuid().ToString(),
+                ExpiresIn = DateTime.Now.Minute,
+                RefreshToken = Guid.NewGuid().ToString(),
+                TokenType = Guid.NewGuid().ToString()
+            };
+
+            var client = new ApiClient("ClientId", "ClientSecret");
+            client.SetOAuth2Token(oAuth2Token);
+            Assert.Same(oAuth2Token, client.OAuth2Token);
+        }
+
+        [Fact]
+        public void OAuth2Token_SetByClientIdConstructor_AreSame()
+        {
+            var oAuth2Token = new OAuth2Token
+            {
+                AccessToken = Guid.NewGuid().ToString(),
+                AccountId = Guid.NewGuid().ToString(),
+                AccountUsername = Guid.NewGuid().ToString(),
+                ExpiresIn = DateTime.Now.Minute,
+                RefreshToken = Guid.NewGuid().ToString(),
+                TokenType = Guid.NewGuid().ToString()
+            };
+
+            var client = new ApiClient("ClientId");
+            client.SetOAuth2Token(oAuth2Token);
+            Assert.Same(oAuth2Token, client.OAuth2Token);
+        }
+
+        [Fact]
+        public void OAuth2Token_SetBySetOAuth2Token_AreSame()
+        {
+            var oAuth2Token = new OAuth2Token
+            {
+                AccessToken = Guid.NewGuid().ToString(),
+                AccountId = Guid.NewGuid().ToString(),
+                AccountUsername = Guid.NewGuid().ToString(),
+                ExpiresIn = DateTime.Now.Minute,
+                RefreshToken = Guid.NewGuid().ToString(),
+                TokenType = Guid.NewGuid().ToString()
+            };
+
+            var client = new ApiClient("ClientId", "ClientSecret");
+
+            Assert.Null(client.OAuth2Token);
+            client.SetOAuth2Token(oAuth2Token);
+            Assert.Same(oAuth2Token, client.OAuth2Token);
+        }
+
+        [Fact]
+        public void OAuth2Token_SetBySetOAuth2Token_Null()
+        {
+            var oAuth2Token = new OAuth2Token
+            {
+                AccessToken = Guid.NewGuid().ToString(),
+                AccountId = Guid.NewGuid().ToString(),
+                AccountUsername = Guid.NewGuid().ToString(),
+                ExpiresIn = DateTime.Now.Minute,
+                RefreshToken = Guid.NewGuid().ToString(),
+                TokenType = Guid.NewGuid().ToString()
+            };
+
+            var client = new ApiClient("ClientId", "ClientSecret");
+
+            Assert.Null(client.OAuth2Token);
+            client.SetOAuth2Token(oAuth2Token);
+            Assert.Same(oAuth2Token, client.OAuth2Token);
+            client.SetOAuth2Token(null);
+            Assert.Null(client.OAuth2Token);
+        }
+    }
+}

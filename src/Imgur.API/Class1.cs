0aa37151cfdf26a9422ed4ad90be4d8ed77eee85
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2020-08-03 19:03:48 +0100

    Refactoring.

---
+++ src/Imgur.API/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Imgur.API
+{
+    public class Class1
+    {
+    }
+}
---
+++ tests/Imgur.API.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Imgur.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

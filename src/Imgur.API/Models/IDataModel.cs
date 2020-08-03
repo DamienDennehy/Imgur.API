0d2732ac025bc8f14a31ccadb29701c40db4ffc7
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2020-08-03 18:47:17 +0100

    Refactoring.

--- src/Imgur.API/Models/IDataModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Imgur.API.Models
-{
-    /// <summary>
-    ///     Base Interface for models.
-    /// </summary>
-    public interface IDataModel
-    {
-    }
-}
--- src/Imgur.API/Models/IGalleryItem.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Imgur.API.Models
-{
-    /// <summary>
-    ///     Any gallery type such as GalleryImage or GalleryAlbum.
-    /// </summary>
-    public interface IGalleryItem : IDataModel
-    {
-    }
-}

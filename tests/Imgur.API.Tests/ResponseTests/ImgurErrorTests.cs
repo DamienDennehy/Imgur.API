617396c9b7dca814cd82ce048bca3ebda6bf0839
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2020-08-07 01:50:22 +0100

    Create ImgurErrorTests.cs

---
+++ tests/Imgur.API.Tests/ResponseTests/ImgurErrorTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Text.Json;
+using Imgur.API.Models;
+using Imgur.API.ResponseConverters;
+using Xunit;
+
+namespace Imgur.API.Tests.ResponseTests
+{
+    public class ImgurErrorTests
+    {
+        [Fact]
+        public void ConvertResponse_WithErrorResponse_ReturnsImgurError()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var errorResponse = JsonSerializer.Deserialize<Basic<ImgurError>>(Mocks.MockErrorResponses.Error, options);
+            Assert.NotNull(errorResponse);
+            Assert.IsType<Basic<ImgurError>>(errorResponse);
+            Assert.Equal(1203, errorResponse.Status);
+        }
+    }
+}

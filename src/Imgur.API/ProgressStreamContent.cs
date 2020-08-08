fbfb77b6bde41e70c2d9223f4d4d50e8df693353
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2020-08-08 01:33:17 +0100

    Refactoring.

---
+++ src/Imgur.API/ProgressStreamContent.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Imgur.API
+{
+    internal class ProgressStreamContent : HttpContent
+    {
+        private const int DefaultBufferSize = 4096;
+        private readonly int _bufferSize = DefaultBufferSize;
+        private readonly Stream _content;
+        private readonly IProgress<int> _progress;
+        private readonly CancellationToken _cancellationToken;
+
+        internal ProgressStreamContent(Stream content,
+                                       IProgress<int> progress) : this(content,
+                                                                       progress,
+                                                                       DefaultBufferSize)
+        {
+        }
+
+        internal ProgressStreamContent(Stream content,
+                                       IProgress<int> progress,
+                                       int? bufferSize,
+                                       CancellationToken cancellationToken = default)
+        {
+            _content = content ?? throw new ArgumentNullException(nameof(content));
+            _progress = progress ?? throw new ArgumentNullException(nameof(progress));
+            _bufferSize = bufferSize ?? throw new ArgumentNullException(nameof(bufferSize));
+            _cancellationToken = cancellationToken;
+        }
+
+        internal Task SerializeToProgressStreamAsync(Stream stream,
+                                                     TransportContext context)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException(nameof(stream));
+            }
+
+            return SerializeToStreamAsync(stream, context);
+        }
+
+        protected override async Task SerializeToStreamAsync(Stream stream,
+                                                             TransportContext context)
+        {
+            var buffer = new byte[_bufferSize];
+
+            using (_content)
+            {
+                _progress.Report(0);
+
+                while (true)
+                {
+                    var length = await _content.ReadAsync(buffer,
+                                                          0,
+                                                          buffer.Length,
+                                                          _cancellationToken)
+                                               .ConfigureAwait(false);
+                    if (length <= 0)
+                    {
+                        break;
+                    }
+
+                    await stream.WriteAsync(buffer,
+                                            0,
+                                            length,
+                                            _cancellationToken)
+                                .ConfigureAwait(false);
+
+                    _progress.Report(length);
+                }
+            }
+        }
+
+        protected override bool TryComputeLength(out long length)
+        {
+            length = _content.Length;
+            return true;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _content.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ src/Imgur.API/RequestBuilders/ImageRequestBuilder.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+
+namespace Imgur.API.RequestBuilders
+{
+    internal static class ImageRequestBuilder
+    {
+        internal static HttpRequestMessage UpdateImageRequest(string url,
+                                                              string title = null,
+                                                              string description = null)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            var parameters = new Dictionary<string, string>();
+
+            if (title != null)
+            {
+                parameters.Add("title", title);
+            }
+
+            if (description != null)
+            {
+                parameters.Add("description", description);
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new FormUrlEncodedContent(parameters.ToArray())
+            };
+
+            return request;
+        }
+
+        internal static HttpRequestMessage UploadImageStreamRequest(string url,
+                                                                    Stream image,
+                                                                    string album = null,
+                                                                    string name = null,
+                                                                    string title = null,
+                                                                    string description = null,
+                                                                    IProgress<int> progressBytes = null)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            if (image == null)
+            {
+                throw new ArgumentNullException(nameof(image));
+            }
+
+            var content = new MultipartFormDataContent($"{DateTime.UtcNow.Ticks}")
+            {
+                {new StringContent("file"), "type"}
+            };
+
+            if (progressBytes != null)
+            {
+                content.Add(new ProgressStreamContent(image, progressBytes), "image");
+            }
+            else
+            {
+                content.Add(new StreamContent(image), "image");
+            }
+
+            if (!string.IsNullOrWhiteSpace(album))
+            {
+                content.Add(new StringContent(album), "album");
+            }
+
+            if (name != null)
+            {
+                content.Add(new StringContent(name), "name");
+            }
+
+            if (title != null)
+            {
+                content.Add(new StringContent(title), "title");
+            }
+
+            if (description != null)
+            {
+                content.Add(new StringContent(description), "description");
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = content
+            };
+
+            return request;
+        }
+
+        internal static HttpRequestMessage UploadImageUrlRequest(string url,
+                                                                 string imageUrl,
+                                                                 string albumId = null,
+                                                                 string name = null,
+                                                                 string title = null,
+                                                                 string description = null)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                throw new ArgumentNullException(nameof(url));
+            }
+
+            if (string.IsNullOrWhiteSpace(imageUrl))
+            {
+                throw new ArgumentNullException(nameof(imageUrl));
+            }
+
+            var parameters = new Dictionary<string, string>
+            {
+                {"type", "URL"},
+                {"image", imageUrl}
+            };
+
+            if (!string.IsNullOrWhiteSpace(albumId))
+            {
+                parameters.Add("album", albumId);
+            }
+
+            if (name != null)
+            {
+                parameters.Add("name", name);
+            }
+
+            if (title != null)
+            {
+                parameters.Add("title", title);
+            }
+
+            if (description != null)
+            {
+                parameters.Add("description", description);
+            }
+
+            var request = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new FormUrlEncodedContent(parameters.ToArray())
+            };
+
+            return request;
+        }
+    }
+}

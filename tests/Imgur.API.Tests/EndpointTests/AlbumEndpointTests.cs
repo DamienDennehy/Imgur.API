9e3c667f9d4bc1acadce6fc2571317a3aff4f886
Damien Dennehy <DamienDennehy@users.noreply.github.com>
2020-08-16 22:02:15 +0100

    Update AlbumEndpointTests.cs

--- tests/Imgur.API.Tests/EndpointTests/AlbumEndpointTests.cs
+++ tests/Imgur.API.Tests/EndpointTests/AlbumEndpointTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -102,6 +103,43 @@ namespace Imgur.API.Tests.EndpointTests
 
             var argNullException = (ArgumentNullException)exception;
             Assert.Equal("imageId", argNullException.ParamName);
+        }
+
+        [Fact]
+        public async Task GetAlbumImagesAsync_Equal()
+        {
+            var mockUrl = "https://api.imgur.com/3/album/nIn0Ntl/images";
+            var mockResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(MockAlbumResponses.GetAlbumImages)
+            };
+
+            var apiClient = new ApiClient("123");
+            var httpClient = new HttpClient(new MockHttpMessageHandler(mockUrl, mockResponse));
+            var endpoint = new AlbumEndpoint(apiClient, httpClient);
+            var response = await endpoint.GetAlbumImagesAsync("nIn0Ntl");
+
+            Assert.NotNull(response);
+            Assert.IsAssignableFrom<IEnumerable<Image>>(response);
+            Assert.Single(response);
+        }
+
+        [Fact]
+        public async Task GetAlbumImagesAsync_WithAlbumNull_ThrowsArgumentNullException()
+        {
+            var apiClient = new ApiClient("123");
+            var endpoint = new AlbumEndpoint(apiClient, new HttpClient());
+
+            var exception = await Record.ExceptionAsync(async () =>
+            {
+                await endpoint.GetAlbumImagesAsync(null);
+            });
+
+            Assert.NotNull(exception);
+            Assert.IsType<ArgumentNullException>(exception);
+
+            var argNullException = (ArgumentNullException)exception;
+            Assert.Equal("albumId", argNullException.ParamName);
         }
     }
 }

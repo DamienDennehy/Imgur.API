482ff2eb9b0ca67cf4543b45c3df1d7685199bb9
Marius <tweety.04.01@gmail.com>
2023-07-27 18:12:44 -0400

    partial album endpoint implementation

---
+++ src/Imgur.API/Endpoints/AlbumEndpoint.cs
@@ -0,0 +1,38 @@
+﻿using Imgur.API.Authentication;
+using Imgur.API.Models;
+using Imgur.API.RequestBuilders;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Imgur.API.Endpoints;
+
+/// <summary>
+/// Album Endpoint.
+/// </summary>
+public class AlbumEndpoint : EndpointBase
+{
+    /// <summary>
+    /// Declares a new instance of the endpoint.
+    /// </summary>
+    /// <param name="apiClient"></param>
+    /// <param name="httpClient"></param>
+    public AlbumEndpoint(IApiClient apiClient, HttpClient httpClient) : base(
+        apiClient, httpClient)
+    {
+    }
+
+    /// <summary>
+    /// Get the list of album images
+    /// </summary>
+    /// <param name="albumId"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async Task<IImage[]> GetImagesAsync(string albumId, CancellationToken cancellationToken = default)
+    {
+        using var request = RequestBuilder.CreateRequest(HttpMethod.Get, $"album/{albumId}/images");
+        var response = await SendRequestAsync<Image[]>(request, cancellationToken);
+        return response;
+    }
+}
